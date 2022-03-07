d3aec7c71e0603924e3483082e318405ef82ae28
dgiot <dgiot@foxmail.com>
2022-03-07 22:35:30 +0800

    :art: (commit) scope

--- src/pages/dashboard/amis/overview.tsx
+++ src/pages/dashboard/amis/overview.tsx
@@ -1,117 +1,17 @@
-import React, { useState, useEffect, ReactNode } from 'react';
+import React from 'react';
 import {
-  Grid,
   Card,
-  Typography,
-  Divider,
-  Skeleton,
-  Link,
 } from '@arco-design/web-react';
-import { useSelector } from 'react-redux';
-import { IconCaretUp } from '@arco-design/web-react/icon';
-import OverviewAreaLine from '@/components/Chart/overview-area-line';
 import axios from 'axios';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import locale from './locale';
-import useLocale from '@/utils/useLocale';
-import styles from './style/overview.module.less';
-import IconCalendar from './assets/calendar.svg';
-import IconComments from './assets/comments.svg';
-import IconContent from './assets/content.svg';
-import IconIncrease from './assets/increase.svg';
 import { render as renderAmis, ToastComponent, AlertComponent } from 'amis';
-import { alert, confirm } from 'amis/lib/components/Alert';
 import { toast } from 'amis/lib/components/Toast';
 import copy from 'copy-to-clipboard';
 import bigscreen from './assets/store/bigscreen.json';
-const { Row, Col } = Grid;
 import 'amis/lib/themes/cxd.css';
 import 'amis/lib/helper.css';
 import 'amis/sdk/iconfont.css';
-type StatisticItemType = {
-  icon?: ReactNode;
-  title?: ReactNode;
-  count?: ReactNode;
-  loading?: boolean;
-  unit?: ReactNode;
-};
-
-function StatisticItem(props: StatisticItemType) {
-  const { icon, title, count, loading, unit } = props;
-  return (
-      <div className={styles.item}>
-        <div className={styles.icon}>{icon}</div>
-        <div>
-          <Skeleton loading={loading} text={{ rows: 2, width: 60 }} animation>
-            <div className={styles.title}>{title}</div>
-            <div className={styles.count}>
-              {count}
-              <span className={styles.unit}>{unit}</span>
-            </div>
-          </Skeleton>
-        </div>
-      </div>
-  );
-}
-
-type DataType = {
-  allContents?: string;
-  liveContents?: string;
-  increaseComments?: string;
-  growthRate?: string;
-  chartData?: { count?: number; date?: string }[];
-  down?: boolean;
-};
-
-type Amis = {
-  amisScoped?: string;
-  theme?: string;
-  locale?: string;
-};
 
 function Overview() {
-  const [data, setData] = useState<DataType>({});
-  const [loading, setLoading] = useState(true);
-  const t = useLocale(locale);
-
-  const userInfo = useSelector((state: any) => state.userInfo || {});
-
-  const fetchData = () => {
-    setLoading(true);
-    axios
-        .get('/api/workplace/overview-content')
-        .then((res) => {
-          setData(res.data);
-        })
-        .finally(() => {
-          setLoading(false);
-        });
-  };
-
-  const loginDgiot = () => {
-    setLoading(true);
-    axios
-        .post(
-            '/iotapi/login',
-            {
-              username: 'dgiot_dev',
-              password: 'dgiot_dev',
-            },
-            { headers: { 'Content-Type': 'text/plain' } }
-        )
-        .then((res) => {
-          setData(res.data);
-        })
-        .finally(() => {
-          setLoading(false);
-        });
-  };
-
-  useEffect(() => {
-    fetchData();
-    loginDgiot();
-  }, []);
   return (
       <Card>
         <div>
