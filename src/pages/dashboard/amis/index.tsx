a6ea697a2258f44c0f0594a185c5737761bea937
dgiot <dgiot@foxmail.com>
2022-03-22 20:13:04 +0800

    fix:dependencies amis del

--- src/pages/dashboard/amis/index.tsx
+++ src/pages/dashboard/amis/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import Editor from './editor';
+// import Editor from './editor';
 // import Overview from './overview';
 // import HookMqtt from '@/components/Hook/index'
 function Amis() {
-  return <Editor />;
+  // return <Editor />;
+  return <div>amis编辑</div>
 }
 
 export default Amis;
